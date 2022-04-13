8866a91e295fdb991277d18fb001a338f35af6c2
Zachary Harper <zharper@uab.edu>
2022-04-13 01:27:28 -0500

    minor changes

--- src/main/java/edu/uab/zharper/retirementplannergui/App.java
+++ src/main/java/edu/uab/zharper/retirementplannergui/App.java
@@ -28,7 +28,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("inputpage"), 640, 480);
+        scene = new Scene(loadFXML("inputpage"), 400, 480);
         stage.setScene(scene);
         stage.show();
     }
