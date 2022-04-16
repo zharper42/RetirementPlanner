aa2f5aaf1fee14f16e273d2e622ea188b3e0e860
Zachary Harper <zharper@uab.edu>
2022-04-16 16:41:07 -0500

    about form

---
+++ src/main/java/edu/uab/zharper/retirementplannergui/AboutFormController.java
@@ -0,0 +1,37 @@
+/*
+ * Group2:       
+ *      Willaim Benefield <wbb31@uab.edu>
+ *      Clayton Dalton <cgdalton@uab.edu>
+ *      Luis Figueroa <alefigue@uab.edu>
+ *      Zachary Harper <zharper@uab.edu>
+ *      Daniel Pineda <mdpineda@uab.edu>
+ *      Joe Wong <jawong1@uab.edu>
+ *
+ * Assignment:  Retirement Planner - EE333 Spring 2022
+ */
+package edu.uab.zharper.retirementplannergui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author mdpineda
+ */
+public class AboutFormController implements Initializable {
+
+  /** Initializes the controller class. */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }
+
+  @FXML
+  private void switchToPrimary() throws IOException {
+    App.setRoot("testPrimary");
+  }
+}
